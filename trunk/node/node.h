8c02db4b7c8078e1261ce91538dd5893a0ad9590
igor.rafael@gmail.com <igor.rafael@gmail.com@c9ea2404-35a9-55dd-69a3-42112d3e4b7a>
2011-05-05 12:37:50 +0000

    Inclusão das estruturas node.c e node.h do SMV.

---
+++ trunk/node/node.h
@@ -0,0 +1,26 @@
+typedef union {
+  int inttype;
+  struct node *nodetype;
+  struct string *strtype;
+  struct bdd *bddtype;
+} value;
+
+typedef struct node{
+  struct node *link;
+  short int type,lineno;
+  value left,right;
+} node_rec,*node_ptr;
+
+node_ptr new_node(),find_node();
+void init_node(),free_node();
+void print_node();
+void fprint_node();
+node_ptr subst_node(),map(),key_node();
+int list_length(),member();
+void free_list();
+node_ptr cons(),car(),cdr(),append(),reverse(),list_minus();
+node_ptr unify_node();
+
+#define NIL ((node_ptr)0)
+#define FAILURE_NODE ((node_ptr)(-1))
+#define ATOM_MAX_LENGTH 256
