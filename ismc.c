6007c840de51c36c58423efd6f9177f11465a7b9
gquadrossilva <gquadrossilva@c9ea2404-35a9-55dd-69a3-42112d3e4b7a>
2011-05-05 16:14:45 +0000

    Arquivo principal atualizado para chamar a tradução AST -> BDD.

--- ismc.c
+++ ismc.c
@@ -2,17 +2,30 @@
 #include <stdio.h>
 
 #include "parser/grammar.h"
+#include "node/node.h"
+
+#include "bdd/ast_to_bdd.h"
 
 // prototype of bison-generated parser function
 int yyparse();
 
-int main(int argc, char * argsv[]){
-	if ((argc > 1) && (freopen(argsv[1], "r", stdin) == NULL)){
-		printf("%s: O arquivo %s não pode ser aberto.", argsv[0], argsv[1]);
+int main(int argc, char *argv[]) {
+    node_ptr root;
+    bdd result;
 
-		exit( EXIT_FAILURE );
+	if ((argc > 1) && (freopen(argv[1], "r", stdin) == NULL)) {
+		printf("%s: O arquivo %s não pode ser aberto.\n", argv[0], argv[1]);
+
+		exit(EXIT_FAILURE);
 	}
 
-	yyparse();
-	return EXIT_SUCCESS;
+    init_bdd();
+
+	root = yyparse();
+    result = eval_bdd(root);
+    
+    bdd_printdot(root);
+    bdd_done();
+
+    return EXIT_SUCCESS;
 }
