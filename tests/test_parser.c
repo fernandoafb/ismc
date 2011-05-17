5f8c03b2ab1cecd94eea718b3447edcbef09ac24
igor.rafael@gmail.com <igor.rafael@gmail.com@c9ea2404-35a9-55dd-69a3-42112d3e4b7a>
2011-05-17 22:05:57 +0000

    Remoção de arquivos de teste desnecessários.

--- tests/test_parser.c
+++
@@ -1,6 +0,0 @@
-#include "../parser/scanner.h"
-
-int main(int argc, char * argsv[]){
-
-}
-
--- tests/test_scanner.c
+++
@@ -1,9 +0,0 @@
-#include "../parser/scanner.h"
-
-int main(int argc, char * argsv[]){
-	if (argc > 1)
-		yyin = fopen(argv[1], "r");
-	yyout = fopen("Output.txt", "w");
-	yylex();
-	return 0;
-}
