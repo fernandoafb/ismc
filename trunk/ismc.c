9217954a4fd5c35f48ba85cab8689f5250dafa7d
fernando.afb@gmail.com <fernando.afb@gmail.com@c9ea2404-35a9-55dd-69a3-42112d3e4b7a>
2011-05-18 00:13:26 +0000

    AST->BDD: NUMBER

--- trunk/ismc.c
+++ trunk/ismc.c
@@ -12,10 +12,7 @@ int yyparse();
 extern node_ptr parse_tree;
 
 int main(int argc, char *argv[]) {
-    node_ptr root;
     bdd result;
-    init_storage();
-    init_assoc();
     init_node();
 	if ((argc > 1) && (freopen(argv[1], "r", stdin) == NULL)) {
 		printf("%s: O arquivo %s não pode ser aberto.\n", argv[0], argv[1]);
@@ -27,6 +24,6 @@ int main(int argc, char *argv[]) {
 
     yyparse();
     eval(parse_tree);
-    
+    //bdd_printall();
     return EXIT_SUCCESS;
 }
