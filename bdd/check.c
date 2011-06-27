fb14c59d48ae79634d82bc42e35f062bf4013493
igor.rafael@gmail.com <igor.rafael@gmail.com@c9ea2404-35a9-55dd-69a3-42112d3e4b7a>
2011-06-27 23:29:25 +0000

    Implementação inicial dos checks.

---
+++ bdd/check.c
@@ -0,0 +1,42 @@
+#include "check.h"
+
+typed_bdd compute_fair(){
+	//fair = EG true
+	return check_EG(new_bdd(bddtrue));
+}
+
+typed_bdd check_AX(typed_bdd f){
+	//TODO: to implement
+}
+
+typed_bdd check_EX(typed_bdd f){
+	//TODO: to implement
+}
+
+typed_bdd check_AU(typed_bdd f, typed_bdd g){
+	//A[f U g] = not E[not g U not f and not g] and not EG not g
+	typed_bdd eu = check_EU(typed_bdd_not(g), typed_bdd_and(typed_bdd_not(f),typed_bdd_not(g)));
+	typed_bdd eg = check_EG(typed_bdd_not(g));
+	return typed_bdd_and(typed_bdd_not(eu), typed_bdd_not(eg));
+}
+
+typed_bdd check_EU(typed_bdd f, typed_bdd g){
+	//TODO: to implement
+}
+
+typed_bdd check_AG(typed_bdd f){
+	//TODO: to implement
+}
+
+typed_bdd check_EG(typed_bdd f){
+	//TODO: to implement
+}
+
+
+typed_bdd check_AF(typed_bdd f){
+	//TODO: to implement
+}
+
+typed_bdd check_EF(typed_bdd f){
+	//TODO: to implement
+}
